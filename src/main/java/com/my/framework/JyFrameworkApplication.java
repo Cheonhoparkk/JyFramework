05b0c2fdf684f6c4d908532be1f434dad4469b61
jypar <jypar@jypark>
2026-02-11 13:42:58 +0900

    Init: 프로젝트 초기 생성

---
+++ src/main/java/com/my/framework/JyFrameworkApplication.java
@@ -0,0 +1,13 @@
+package com.my.framework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JyFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JyFrameworkApplication.class, args);
+	}
+
+}
